f5d303aa21b52d76c3c0ddcf9f89c6980e177dfd
imendezmt <ivan.mendez.mt@correo.usa.edu.co>
2021-12-14 23:31:12 -0500

    first commit

---
+++ src/main/java/Reto2_Web/InterfaceCleaningProduct.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto2_Web;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceCleaningProduct extends MongoRepository<CleaningProduct, Integer> {
+    
+}
